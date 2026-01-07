37905bf06ca453e56f42ddfcb2375cf4af996f98
ismail baoud <ibaoud46@egmail.com>
2026-01-07 22:36:34 +0100

    add initial application structure with routing and components

---
+++ front-end/albaraka/src/app/features/operations/operation/operation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operation',
+  imports: [],
+  templateUrl: './operation.html',
+  styleUrl: './operation.css',
+})
+export class Operation {
+
+}
