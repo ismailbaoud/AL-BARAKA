7946841569ecac305dda5aad873da732f4795a4b
ismail baoud <ibaoud46@egmail.com>
2026-01-08 22:38:24 +0100

    update app configuration and routes for improved HTTP client integration

--- front-end/albaraka/src/app/app.routes.ts
+++ front-end/albaraka/src/app/app.routes.ts
@@ -1,16 +1,6 @@
-import { Register } from './features/authentication/register/register';
 import { Routes } from '@angular/router';
-import { App } from './app';
-import { Login } from './features/authentication/login/login';
-import { AdminDashboard } from './features/dashboard/admin-dashboard/admin-dashboard';
-import { ClientDashboard } from './features/dashboard/client-dashboard/client-dashboard';
-import { AgentDashboard } from './features/dashboard/agent-dashboard/agent-dashboard';
+import { Login } from './features/auth/components/login/login';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
-  { path: 'dashboard/admin', component: AdminDashboard },
-  { path: 'dashboard/client', component: ClientDashboard },
-  { path: 'dashboard/agent', component: AgentDashboard },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'login', component:Login }
 ];
