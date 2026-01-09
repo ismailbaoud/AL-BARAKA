a2d0f2b3b00811e4b47387c69b80f93a3cd2e4a1
ismail baoud <ibaoud46@egmail.com>
2026-01-09 21:00:52 +0100

    feat(navbar): add Navbar component with styling and user logout functionality

--- src/main/java/com/ismail/al_baraka/controller/AuthController.java
+++ src/main/java/com/ismail/al_baraka/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.ismail.al_baraka.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +15,7 @@ import com.ismail.al_baraka.service.impliment.AuthServiceImpl;
 import com.ismail.al_baraka.service.impliment.UserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
